6442c8536be8a0c11cfc3a332e3f76836ff93f9c
morr1smo10 <morr1smo107@gmail.com>
2024-02-26 13:20:03 -0800

    finish tic tac toe

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function createPlayer (input_name, input_identifier) {
 
 // gameboard object
 const gameboard = (function () {
-  let board = ["X", "O", "", "", "", "", "", "", ""];
+  let board = ["", "", "", "", "", "", "", "", ""];
 
   const reset = () => {
     board = ["", "", "", "", "", "", "", "", ""];
@@ -43,17 +43,17 @@ const gameboard = (function () {
     }
     board[index] = identifier;
     if (checkwin()) {
-      reset();
+      // reset();
       return 0; // game end with one player wins
     } else if (checkfull()){
-      reset();
+      // reset();
       return 1; // game end with both player tie
     } else {
       return 2; // game continues
     }
   };
 
-  return {makemove, getBoard};
+  return {makemove, getBoard, reset};
 })();
 
 // gamecontrol object
@@ -69,6 +69,16 @@ const gamecontrol = (function () {
       current = player1;
     }
   };
+
+  const setname1 = (new_name) => {
+    player1.name = new_name;
+  }
+
+  const setname2 = (new_name) => {
+    player2.name = new_name;
+  }
+
+  const getcurrentplayer = () => current;
 
   const taketurn = (index) => {
     let res = gameboard.makemove(current.identifier, index);
@@ -88,19 +98,66 @@ const gamecontrol = (function () {
     return res;
   };
 
-  return {taketurn};
+  return {taketurn, getcurrentplayer, setname1, setname2};
 })();
 
 // displaycontrol object
 const displaycontrol = (function () {
+  const turn_message = document.getElementById("turn_message");
+  const result_message = document.getElementById("result_message");
+
   const renderboard = () => {
     for (var i = 0; i < 9; i++) {
       const identifier = gameboard.getBoard()[i];
       const grid = document.getElementById(i);
-      console.log(identifier);
       grid.innerHTML = `<p>${identifier}</p>`;
     }
   };
 
-  return {renderboard};
+  const addClickListener = () => {
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid) => {
+      grid.addEventListener("click", (e) => {
+        const index = e.target.id;
+        const res = gamecontrol.taketurn(index);
+        if (res === 2) {
+          turn_message.textContent = `${gamecontrol.getcurrentplayer().name}'s turn`;
+          result_message.textContent = "";
+        } else if (res === 1){
+          turn_message.textContent = "Game end with a tie";
+          result_message.textContent = "Click start to play a new game";
+        } else if (res === 0) {
+          turn_message.textContent = `Game end with ${gamecontrol.getcurrentplayer().name} wins`;
+          result_message.textContent = "Click start to play a new game";
+        } else if (res === 3) {
+          turn_message.textContent = `${gamecontrol.getcurrentplayer().name}'s turn`;
+          result_message.textContent = "Don't play in spots that are already taken";
+        }
+        renderboard();
+      });
+    });
+  };
+
+  const startdisplay = () => {
+    const button = document.getElementById("start_button");
+    button.addEventListener("click", () => {
+      turn_message.textContent = `${gamecontrol.getcurrentplayer().name} 's turn`;
+      result_message.textContent = "";
+      gameboard.reset();
+      renderboard();
+    });
+  };
+
+  const setname = () => {
+    const name1 = prompt("Type the name of the player 1");
+    const name2 = prompt("Type the name of the player 2");
+    gamecontrol.setname1(name1);
+    gamecontrol.setname2(name2);
+  };
+    
+  return {addClickListener, startdisplay, setname};
 })();
+
+displaycontrol.setname();
+displaycontrol.startdisplay();
+displaycontrol.addClickListener();
